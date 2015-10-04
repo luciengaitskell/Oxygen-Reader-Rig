80bf73f887802c6c2f8cebd5360342f6fbd9d4d8
Lucien Gaitskell <lucien@gaitskell.com>
2015-10-03 23:09:10 -0400

    removed threading (continuing to use thruster safety system)

--- blueESC-Thruster/blueESCDrive/blueESCDrive.py
+++ blueESC-Thruster/blueESCDrive/blueESCDrive.py
@@ -3,24 +3,24 @@ import threading
 DEFAULT={'busNumb':1, 'address':0x29}
 ADDRESSES={'throttle':0x00}
 
-class blueESCDrive(threading.Thread):
+class blueESCDrive:
     def __init__(self, address=DEFAULT['address'], busNumb=DEFAULT['busNumb']):
         self.BUS = smbus.SMBus(busNumb)
         self.ADDRESS=address
-    def run():
-        pass
+
     def IOWord(self, address, inputValue=None):
         if inputValue==None:
             return self.BUS.read_byte_data(self.ADDRESS, address)
         else:
             self.BUS.write_word_data(self.ADDRESS, address, inputValue)
 
-    def power(self, power=None):
-        if power==None:
+    def power(self, inputPower=None):
+        if inputPower==None:
             return self.IOWord(ADDRESSES['throttle'])
         else:
-            if not type(power) is int:
-                raise TypeError("(setPower) power requires 'int' not '" + type(power).__name__ + "'")
-            """if self.power() == 0:
+            if not type(inputPower) is int:
+                raise TypeError("inputPower requires 'int' not '" + type(inputPower).__name__ + "'")
+            """if self.inputPower() == 0:
                 self.IOWord(ADDRESSES['throttle'], 0)"""
-            self.IOWord(ADDRESSES['throttle'], power)
+            self.IOWord(ADDRESSES['throttle'], 0)
+            self.IOWord(ADDRESSES['throttle'], inputPower)
