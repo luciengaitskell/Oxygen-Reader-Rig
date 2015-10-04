34299f73e6e09ebbd166ed899d98665b0256bf3f
lucien.gaitskell <lucien@gaitskell.com>
2015-10-03 23:01:20 -0400

    fixed small esc drive errors

--- blueESC-Thruster/blueESCDrive/blueESCDrive.py
+++ blueESC-Thruster/blueESCDrive/blueESCDrive.py
@@ -1,25 +1,26 @@
 import smbus
+import threading
 DEFAULT={'busNumb':1, 'address':0x29}
 ADDRESSES={'throttle':0x00}
 
-class gpsRead(threading.Thread):
-    def __init__(self, address=default.address, busNumb=default.busNumb):
+class blueESCDrive(threading.Thread):
+    def __init__(self, address=DEFAULT['address'], busNumb=DEFAULT['busNumb']):
         self.BUS = smbus.SMBus(busNumb)
         self.ADDRESS=address
     def run():
         pass
     def IOWord(self, address, inputValue=None):
         if inputValue==None:
-            self.BUS.read_byte_data(self.ADDRESS, address)
+            return self.BUS.read_byte_data(self.ADDRESS, address)
         else:
-            self.BUS.write_word_data(self.ADDRESS, address, importValue)
+            self.BUS.write_word_data(self.ADDRESS, address, inputValue)
 
     def power(self, power=None):
         if power==None:
-            return self.BUS.IOWord(self.ADDRESSES.throttle)
+            return self.IOWord(ADDRESSES['throttle'])
         else:
             if not type(power) is int:
                 raise TypeError("(setPower) power requires 'int' not '" + type(power).__name__ + "'")
-            if self.getPower() == 0
-                self.IOWord(ADDRESSES.throttle, 0)
-            self.IOWord(ADDRESSES.throttle, power)
+            """if self.power() == 0:
+                self.IOWord(ADDRESSES['throttle'], 0)"""
+            self.IOWord(ADDRESSES['throttle'], power)
